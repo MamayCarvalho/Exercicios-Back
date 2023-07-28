4923f69c12c1eefa638f196654b81966377acc8f
MamayCarvalho <115594676+MamayCarvalho@users.noreply.github.com>
2023-07-27 21:45:28 -0300

    Update ExercicioQuestao02.java
    Ajuste Locale

--- ExercicioQuestao02.java
+++ ExercicioQuestao02.java
@@ -1,8 +1,11 @@
 package Exercicios;
 
+import java.util.Locale;
 public class ExercicioQuestao02 {
 
 	public static void main(String[] args) {
+		Locale locale=new Locale("en","US");
+		Locale.setDefault(locale);
 		double pi = 3.14159;
 		double valor01 = 150;
 		double raio = 0;
